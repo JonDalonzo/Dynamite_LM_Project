2b703aa5f3f6e6da6dca8a342394ac84c4b1a86e
Jonathan D'Alonzo <Jdalonz19@gmail.com>
2017-07-25 10:32:30 -0400

    update

--- Driver.java
+++ Driver.java
@@ -15,28 +15,45 @@ public class Driver {
       
     public static void main(String[] args) throws Exception {
 
-        Random rand = new Random(); 
-        double[][] initdata = new double[0][0];
+//        Random rand = new Random(); 
+//        double[][] initdata = new double[0][0];
+        
         DynamiteGUI gui = new DynamiteGUI();
-        
-        //PROCESS
-            //1. Get simulated random robot y values
-        double[] simData = gui.fillArrayWithRand(100, 1);
-            //2. Store them in the gui
-        gui.setRobotYData(simData);
-            //3. Get simulated sensor projection data
-        double[] simData2 = gui.fillArrayWithRand(50, 1);
-            //4. Store sensor data
-        gui.setSensorData(simData2);
-            //5. Calculate the y data of the canyon wall with current data
-        gui.calculateCanyonYData();
-        
+        test(gui);
         // Create Chart
         XYChart chart = gui.getChart();
         // Show it
         final SwingWrapper<XYChart> sw = new SwingWrapper<XYChart>(chart);
         sw.displayChart();
     }
+    
+    public static void test(DynamiteGUI gui) {
+        //PROCESS
+            //1. Get simulated random robot y values
+        double[] simData = fillArrayWithRand(gui, 100, 1);
+            //2. Store them in the gui
+        gui.setRobotYData(simData);
+            //3. Get simulated sensor projection data
+        double[] simData2 = fillArrayWithRand(gui, 50, 1);
+            //4. Store sensor data
+        gui.setSensorData(simData2);
+            //5. Calculate the y data of the canyon wall with current data
+        try {
+            gui.calculateCanyonYData();
+        } catch (Exception e) {
+            System.out.println("Exception thrown: calculateCanyonYData, " + e.getMessage());
+        }
+    }
+
+    public static double[] fillArrayWithRand(DynamiteGUI gui, double max, double min) {
+        Random rand = new Random();
+        double[] randValues = new double[gui.MAXDISTANCE];
+        for (int i = 0; i < gui.MAXDISTANCE; i++) {
+            randValues[i] = min + (max - min) * rand.nextDouble(); // gets random value between 1 and 100
+        }
+        return randValues;
+    }
+
 }
 
 
