d3a373f20035c86608a95f3a1eb4b5dbcb92e121
Jonathan D'Alonzo <Jdalonz19@gmail.com>
2017-07-31 14:41:04 -0400

    Initial creation/update

---
+++ Calculator.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dynamite;
+
+/**
+ *
+ * @author jonathandalonzo
+ */
+public class Calculator {
+    
+    
+    
+    
+    public double findDistanceTravelled(double leftMotor, double rightMotor) {
+        
+        double distance = 0.00;
+        
+        
+        return distance;
+    }
+    
+    public double changeInY(double distance, double theta) {
+        return distance * Math.sin(theta);
+    }
+    
+    public double changeInX(double distance, double theta) {
+        return distance * Math.cos(theta);
+    }
+    
+}
