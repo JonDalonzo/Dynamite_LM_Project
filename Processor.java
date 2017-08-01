9340be23a46646574fb6ec23f8e8c12e05806a65
Jonathan D'Alonzo <Jdalonz19@gmail.com>
2017-08-01 15:04:21 -0400

    final commit v1.0

--- Processor.java
+++ Processor.java
@@ -7,6 +7,8 @@ package dynamite;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -36,60 +38,84 @@ public class Processor {
         String line = "START OF FILE";
         boolean run = true;
         
-        try {
-            FileReader fr = new FileReader(filePath);
-            BufferedReader textReader = new BufferedReader(fr);
-            
-            do {
-                line = textReader.readLine();
-                if (line.contains("done") || line == "") {
-                    run = false;
-                }
-                else {
-                    lineArray = line.split(", ");
-                    int j = 0;
-                    for (int i = data.length; i < lineArray.length; i++) {
-                        data[i] = lineArray[j];
-                        j++;
-                    }
-                }
-            } while (line != null && run);
-            
-            if (data.length != 0 || data == null) {
-                //separate data
-                separateData(data);
-                //set the amount of times data was taken during the journey
-                driver.setMaxDataReadings(leftMotor.length);
-                //calculate the distance travelled from previous data retrieval
-                distTravelled = calc.findDistanceTravelled(leftMotor, rightMotor);
-                //calculate the change in y from previous data retrieval 
-                changeInY = calc.changeInY(distTravelled, zAxis, sonicDistance[0]);
-                //calculate the change in x from previous data retrieval
-                changeInX = calc.changeInX(distTravelled, zAxis);
-                //calculate the new y coordinates and then set them
-                double[] robotY = calc.findNewCoordinates(changeInY);
-                driver.setRobotY(robotY);
-                //calculate the new x coordinates and then store them
-                driver.setXValues(calc.findNewCoordinates(changeInX));
-                //calculate the canyons y values then store them
-                driver.setCanyonY(calc.calculateCanyonYData(robotY, sonicDistance, driver.getMaxDataReadings()));
-            }
-            
-            fr.close();
-            textReader.close();
-            
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
+        data = parseFile(filePath);
+        initializeArray(data.length/4);
+        
+        if (data.length != 0 || data == null) {
+            //separate data
+            separateData(data);
+            //set the amount of times data was taken during the journey
+            driver.setMaxDataReadings(leftMotor.length);
+            //find theta values
+//            findTheta();
+            //calculate the distance travelled from previous data retrieval
+            distTravelled = calc.findDistanceTravelled(leftMotor, rightMotor);
+            //calculate the change in y from previous data retrieval 
+            changeInY = calc.changeInY(distTravelled, zAxis, sonicDistance[0]);
+            //calculate the change in x from previous data retrieval
+            changeInX = calc.changeInX(distTravelled, zAxis);
+            //calculate the new y coordinates and then set them
+            double[] robotY = calc.findNewCoordinates(changeInY);
+            //calculate the new x coordinates and then store them
+            double[] xValues = calc.findNewCoordinates(changeInX);
+            //calculate the canyons y values then store them
+            double[] canyonY = calc.calculateCanyonYData(robotY, sonicDistance, driver.getMaxDataReadings());
+            double minimumCanyonY = findMin(canyonY);
+            //format all data sets to put them on the plot
+            robotY = formatValues(robotY, minimumCanyonY);;
+            canyonY = formatValues(canyonY, minimumCanyonY);
+            driver.setMinAndMax(findMin(canyonY), findMax(robotY));
+            driver.setRobotY(robotY);
+            driver.setXValues(xValues);
+            driver.setCanyonY(canyonY);
         }
+    }
+    
+    public double findMin(double[] data) {
+        double min = 100.00;
+        for (int i = 0; i < data.length; i++) {
+            if (data[i] < min) {
+                min = data[i];
+            }
+        }
+        return min;
+    }
+    
+    public double findMax(double[] data) {
+        double min = 0.00;
+        for (int i = 0; i < data.length; i++) {
+            if (data[i] > min) {
+                min = data[i];
+            }
+        }
+        return min;
+    }
+    
+    public double[] formatValues(double[] dataSet, double bufferValue) {
+        if (bufferValue < 0) {
+            bufferValue = -(bufferValue);
+        }
+        for (int i = 0; i < dataSet.length; i++) {
+            dataSet[i] += bufferValue;
+        }
+        return dataSet;
+    }
+    
+    private void initializeArray(int size) {
+        leftMotor = new double[size];
+        rightMotor = new double[size];
+        zAxis = new double[size];
+        sonicDistance = new double[size];
     }
     
     private void separateData(String[] data) {
         
         int j = 0;
+        int size = (data.length / 4);
         boolean run = true;
-        for (int i = 0; i < data.length || run; i++) {
+        for (int i = 0; j < size && run; i++) {
             String dataLine = data[i] + data[i+1] + data[i+2] + data[i+3];
-            if (dataLine.contains("done")) {
+            if (dataLine.contains("done") || dataLine == null) {
                 run = false;
             }
             else {
@@ -101,8 +127,42 @@ public class Processor {
             }
         }
     }
-    
-    public void findRobotYData() {
+
+    public String[] parseFile(String filePath) {
         
+        String[] lineArray;
+        String[] results;
+        List <String> data = new ArrayList<String>();
+        String line = "";
+        boolean run = true;
+        
+        try {
+            BufferedReader textReader = new BufferedReader(new FileReader(filePath));
+            
+            do {
+                line = textReader.readLine();
+                if (line.contains("done") || line == null) {
+                    run = false;
+                }
+                else {
+                    lineArray = line.split(", ");
+                    for (int i = 0; i < 4; i++) {
+                        if (lineArray[i] != "") {
+                            data.add(lineArray[i]);
+                        }
+                    }
+                    textReader.readLine(); //consume \n
+                }
+            } while (run);
+            
+            textReader.close();
+            
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        
+        results = new String[data.size()];
+        return data.toArray(results);
     }
+
 }
